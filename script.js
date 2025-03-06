80bd926e4281ad10c9039a4671df60e714feed18
Museyib Huseynov <museyib.huseynov.dev@gmail.com>
2025-03-06 18:12:53 +0400

    update function updated

--- script.js
+++ script.js
@@ -46,12 +46,19 @@ let x,
   alpha = 100,
   mapType = 'contour',
   algorithm = 'kriging (spherical)',
-  color = 'saturation',
-  contour_coloring = 'fill',
+  contour_map_color = 'saturation',
+  contour_map_coloring = 'fill',
+  contour_line_color = 'grey',
+  contour_line_width = 0.5,
+  contour_line_smoothing = 0,
   show_contour_labels = true,
   zStart = 0,
   zEnd = 100,
-  zStepSize = 4;
+  zStepSize = 4,
+  xMinExtend = 1,
+  xMaxExtend = 1,
+  yMinExtend = 1,
+  yMaxExtend = 1;
 
 const fileInput = document.getElementById('fileInput');
 const gridSizeDropdown = document.getElementById('gridSizeDropdown');
@@ -82,8 +89,8 @@ fileInput.addEventListener('change', function (e) {
     let yMin = Math.min(...y) - 1;
     let yMax = Math.max(...y) + 1;
 
-    let xRange = [xMin, xMax];
-    let yRange = [yMin, yMax];
+    let xRange = [xMin - 1000, xMax + 1000];
+    let yRange = [yMin - 1000, yMax + 1000];
 
     let gridSize = 100;
 
@@ -100,10 +107,12 @@ fileInput.addEventListener('change', function (e) {
     );
 
     let contourData = {
+      name: 'contour map',
+      hoverinfo: 'z',
       x: xGrid,
       y: yGrid,
       z: zGrid,
-      type: 'contour',
+      type: 'heatmap',
       colorscale: [
         ['0.0', 'rgb(165,0,38)'],
         ['0.111111111111', 'rgb(215,48,39)'],
@@ -117,19 +126,20 @@ fileInput.addEventListener('change', function (e) {
         ['1.0', 'rgb(49,54,149)'],
       ],
       contours: {
-        coloring: 'fill',
+        coloring: 'heatmap',
         showlabels: true,
         labelfont: {
-          size: 12, // Adjust label size
+          size: 10, // Adjust label size
           color: 'black',
         },
         start: 0,
         end: 100,
-        size: 3,
+        size: 1,
       },
       line: {
-        width: '1',
-        smoothing: 1,
+        color: 'grey',
+        // width: '1',
+        smoothing: '0',
       },
       zmin: 0,
       zmax: 100,
@@ -143,21 +153,25 @@ fileInput.addEventListener('change', function (e) {
       marker: { color: 'black', size: 8 },
       text: wellNames, // Display well names
       textposition: 'top left',
-      textfont: { size: 10, color: 'red' },
+      textfont: { size: 10, color: 'black' },
     };
 
     let layout = {
       title: {
         text: 'Isomap',
-        font: { size: 18, color: 'black' },
+        font: { size: 18, color: 'grey' },
       },
       xaxis: {
-        visible: false,
+        visible: true,
         // title: { text: 'X', font: { size: 14, color: 'black' } },
+        tickformat: '.0f',
+        range: [xRange[0], xRange[1]],
       },
       yaxis: {
-        visible: false,
+        visible: true,
         // title: { text: 'Y', font: { size: 14, color: 'black' } },
+        tickformat: '.0f',
+        range: [yRange[0], yRange[1]],
       },
     };
 
@@ -171,18 +185,23 @@ gridSizeDropdown.addEventListener('change', function (e) {
   console.log(gridSize);
 });
 
-function kriging_method(model) {
+// function kriging_method(model) {
+//   let variogram = kriging.train(z, x, y, model, sigma2, alpha);
+//   return yGrid.map((yVal) =>
+//     xGrid.map((xVal) => kriging.predict(xVal, yVal, variogram))
+//   );
+// }
+
+function kriging_method(model, xValue, yValue) {
   let variogram = kriging.train(z, x, y, model, sigma2, alpha);
-  return yGrid.map((yVal) =>
-    xGrid.map((xVal) => kriging.predict(xVal, yVal, variogram))
-  );
+  return kriging.predict(xValue, yValue, variogram);
 }
 
 function updateMap(fn) {
-  let xMin = Math.min(...x) - 1;
-  let xMax = Math.max(...x) + 1;
-  let yMin = Math.min(...y) - 1;
-  let yMax = Math.max(...y) + 1;
+  let xMin = Math.min(...x) - xMinExtend;
+  let xMax = Math.max(...x) + xMaxExtend;
+  let yMin = Math.min(...y) - yMinExtend;
+  let yMax = Math.max(...y) + yMaxExtend;
 
   let xRange = [xMin, xMax];
   let yRange = [yMin, yMax];
@@ -195,33 +214,43 @@ function updateMap(fn) {
     { length: gridSize },
     (_, i) => yRange[0] + (i * (yRange[1] - yRange[0])) / gridSize
   );
-  let zGrid;
+  let zGrid = yGrid.map((yVal) =>
+    xGrid.map((xVal) => {
+      if (algorithm == 'kriging (exponential)') {
+        return kriging_method('exponential', xVal, yVal);
+      } else if (algorithm == 'kriging (spherical)') {
+        return kriging_method('exponential', xVal, yVal);
+      } else if (algorithm == 'kriging (gaussian)') {
+        return kriging_method('gaussian', xVal, yVal);
+      }
+    })
+  );
 
-  if (algorithm == 'kriging (exponential)') {
-    zGrid = kriging_method('exponential');
-  } else if (algorithm == 'kriging (spherical)') {
-    zGrid = kriging_method('exponential');
-  } else if (algorithm == 'kriging (gaussian)') {
-    zGrid = kriging_method('gaussian');
-  }
-
-  let contourData = {
+  let mapData = {
+    type: mapType == 'contour' ? 'contour' : 'heatmap',
     x: xGrid,
     y: yGrid,
     z: zGrid,
-    type: mapType,
+    hoverinfo: 'z',
     colorscale:
-      color == 'saturation'
+      contour_map_color == 'saturation'
         ? saturationColor
-        : color == 'pressure'
+        : contour_map_color == 'pressure'
         ? pressureColor
         : depthColor,
     contours: {
-      coloring: contour_coloring,
+      coloring: contour_map_coloring,
       showlabels: show_contour_labels,
       start: zStart,
       end: zEnd,
       size: zStepSize,
     },
+    line: {
+      color: contour_line_color,
+      width: contour_line_width,
+      smoothing: contour_line_smoothing,
+    },
+    zmin: zStart,
+    zmax: zEnd,
   };
 }
