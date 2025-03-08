bbcca426a18e76fae711b64c92829d86858f5385
Museyib Huseynov <museyib.huseynov.dev@gmail.com>
2025-03-09 00:37:36 +0400

    major updates

--- script.js
+++ script.js
@@ -41,6 +41,9 @@ let x,
   y,
   z,
   wellNames,
+  currentWorker = null,
+  lastWorkerParams = null,
+  cachedZGrid = null,
   gridSize = 100,
   sigma2 = 0,
   alpha = 100,
@@ -54,17 +57,28 @@ let x,
   show_contour_labels = true,
   zStart = 0,
   zEnd = 100,
-  zStepSize = 4,
-  xMinExtend = 1,
-  xMaxExtend = 1,
-  yMinExtend = 1,
-  yMaxExtend = 1;
+  zStepSize = 1,
+  xMinExtend = 1000,
+  xMaxExtend = 1000,
+  yMinExtend = 1000,
+  yMaxExtend = 1000,
+  scatter_marker_color = 'black',
+  scatter_marker_size = 8,
+  scatter_marker_text_position = 'top left',
+  scatter_marker_text_size = 12,
+  scatter_marker_text_color = 'black',
+  mapName = 'Iso-Map';
 
+const map = document.getElementById('map');
 const fileInput = document.getElementById('fileInput');
 const gridSizeDropdown = document.getElementById('gridSizeDropdown');
+const loadingIndicator = document.querySelector('.spinner');
+const configurationContainer = document.querySelector('.configuration');
 
 fileInput.addEventListener('change', function (e) {
   let file = e.target.files[0];
+  if (!file) return;
+
   const reader = new FileReader();
   reader.readAsArrayBuffer(file);
   reader.onload = function (e) {
@@ -75,129 +89,29 @@ fileInput.addEventListener('change', function (e) {
     const worksheet = workbook.Sheets[sheetName];
 
     const parsedData = XLSX.utils.sheet_to_json(worksheet);
-    console.log(parsedData);
+
     x = parsedData.map((i) => i.x);
     y = parsedData.map((i) => i.y);
     z = parsedData.map((i) => i.z);
     wellNames = parsedData.map((i) => i.wellNames);
 
-    let model = 'spherical'; // Options: "exponential", "spherical", "gaussian"
-    let variogram = kriging.train(z, x, y, model, sigma2, alpha);
-
-    let xMin = Math.min(...x) - 1;
-    let xMax = Math.max(...x) + 1;
-    let yMin = Math.min(...y) - 1;
-    let yMax = Math.max(...y) + 1;
-
-    let xRange = [xMin - 1000, xMax + 1000];
-    let yRange = [yMin - 1000, yMax + 1000];
-
-    let gridSize = 100;
-
-    let xGrid = Array.from(
-      { length: gridSize },
-      (_, i) => xRange[0] + (i * (xRange[1] - xRange[0])) / gridSize
-    );
-    let yGrid = Array.from(
-      { length: gridSize },
-      (_, i) => yRange[0] + (i * (yRange[1] - yRange[0])) / gridSize
-    );
-    let zGrid = yGrid.map((yVal) =>
-      xGrid.map((xVal) => kriging.predict(xVal, yVal, variogram))
-    );
-
-    let contourData = {
-      name: 'contour map',
-      hoverinfo: 'z',
-      x: xGrid,
-      y: yGrid,
-      z: zGrid,
-      type: 'heatmap',
-      colorscale: [
-        ['0.0', 'rgb(165,0,38)'],
-        ['0.111111111111', 'rgb(215,48,39)'],
-        ['0.222222222222', 'rgb(244,109,67)'],
-        ['0.333333333333', 'rgb(253,174,97)'],
-        ['0.444444444444', 'rgb(254,224,144)'],
-        ['0.555555555556', 'rgb(224,243,248)'],
-        ['0.666666666667', 'rgb(171,217,233)'],
-        ['0.777777777778', 'rgb(116,173,209)'],
-        ['0.888888888889', 'rgb(69,117,180)'],
-        ['1.0', 'rgb(49,54,149)'],
-      ],
-      contours: {
-        coloring: 'heatmap',
-        showlabels: true,
-        labelfont: {
-          size: 10, // Adjust label size
-          color: 'black',
-        },
-        start: 0,
-        end: 100,
-        size: 1,
-      },
-      line: {
-        color: 'grey',
-        // width: '1',
-        smoothing: '0',
-      },
-      zmin: 0,
-      zmax: 100,
-    };
-
-    let scatterData = {
-      x: x,
-      y: y,
-      mode: 'markers+text',
-      type: 'scatter',
-      marker: { color: 'black', size: 8 },
-      text: wellNames, // Display well names
-      textposition: 'top left',
-      textfont: { size: 10, color: 'black' },
-    };
-
-    let layout = {
-      title: {
-        text: 'Isomap',
-        font: { size: 18, color: 'grey' },
-      },
-      xaxis: {
-        visible: true,
-        // title: { text: 'X', font: { size: 14, color: 'black' } },
-        tickformat: '.0f',
-        range: [xRange[0], xRange[1]],
-      },
-      yaxis: {
-        visible: true,
-        // title: { text: 'Y', font: { size: 14, color: 'black' } },
-        tickformat: '.0f',
-        range: [yRange[0], yRange[1]],
-      },
-    };
-
-    Plotly.newPlot('contourMap', [contourData, scatterData], layout);
+    updateMap();
   };
   //
 });
 
 gridSizeDropdown.addEventListener('change', function (e) {
   gridSize = parseInt(this.value);
-  console.log(gridSize);
+  updateMap();
 });
 
-// function kriging_method(model) {
-//   let variogram = kriging.train(z, x, y, model, sigma2, alpha);
-//   return yGrid.map((yVal) =>
-//     xGrid.map((xVal) => kriging.predict(xVal, yVal, variogram))
-//   );
-// }
+function updateMap() {
+  if (currentWorker) {
+    currentWorker.terminate();
+  }
 
-function kriging_method(model, xValue, yValue) {
-  let variogram = kriging.train(z, x, y, model, sigma2, alpha);
-  return kriging.predict(xValue, yValue, variogram);
-}
+  showLoading();
 
-function updateMap(fn) {
   let xMin = Math.min(...x) - xMinExtend;
   let xMax = Math.max(...x) + xMaxExtend;
   let yMin = Math.min(...y) - yMinExtend;
@@ -214,18 +128,32 @@ function updateMap(fn) {
     { length: gridSize },
     (_, i) => yRange[0] + (i * (yRange[1] - yRange[0])) / gridSize
   );
-  let zGrid = yGrid.map((yVal) =>
-    xGrid.map((xVal) => {
-      if (algorithm == 'kriging (exponential)') {
-        return kriging_method('exponential', xVal, yVal);
-      } else if (algorithm == 'kriging (spherical)') {
-        return kriging_method('exponential', xVal, yVal);
-      } else if (algorithm == 'kriging (gaussian)') {
-        return kriging_method('gaussian', xVal, yVal);
-      }
-    })
-  );
 
+  let currentWorkerParams = { xGrid, yGrid, algorithm, sigma2, alpha, x, y, z };
+
+  if (
+    lastWorkerParams &&
+    JSON.stringify(lastWorkerParams) == JSON.stringify(currentWorkerParams)
+  ) {
+    drawMap(cachedZGrid, xGrid, yGrid);
+    configurationContainer.style.display = 'block';
+    return;
+  }
+
+  lastWorkerParams = currentWorkerParams;
+
+  currentWorker = new Worker('webworker.js');
+  currentWorker.postMessage(currentWorkerParams);
+
+  currentWorker.onmessage = function (e) {
+    cachedZGrid = e.data.zGrid;
+    hideLoading();
+    drawMap(cachedZGrid, xGrid, yGrid);
+    configurationContainer.style.display = 'block';
+  };
+}
+
+function drawMap(zGrid, xGrid, yGrid) {
   let mapData = {
     type: mapType == 'contour' ? 'contour' : 'heatmap',
     x: xGrid,
@@ -253,4 +181,64 @@ function updateMap(fn) {
     zmin: zStart,
     zmax: zEnd,
   };
+
+  let scatterData = {
+    x: x,
+    y: y,
+    mode: 'markers+text',
+    type: 'scatter',
+    marker: { color: scatter_marker_color, size: scatter_marker_size },
+    text: wellNames,
+    textposition: scatter_marker_text_position,
+    textfont: {
+      size: scatter_marker_text_size,
+      color: scatter_marker_text_color,
+    },
+  };
+
+  let layout = {
+    title: {
+      text: mapName,
+      font: { size: 18, color: 'black' },
+    },
+    xaxis: {
+      visible: true,
+      tickformat: '.0f',
+      range: [Math.min(...x) - xMinExtend, Math.max(...x) + xMaxExtend],
+    },
+    yaxis: {
+      visible: true,
+      tickformat: '.0f',
+      range: [Math.min(...y) - yMinExtend, Math.max(...y) + yMaxExtend],
+    },
+  };
+
+  const config = {
+    displayModeBar: true,
+    displaylogo: false,
+    scrollZoom: true,
+    modeBarButtonsToRemove: [
+      'pan2d',
+      'select2d',
+      'lasso2d',
+      'zoom2d',
+      'autoScale2d',
+    ],
+    modeBarButtons: [['zoomIn2d', 'zoomOut2d', 'resetScale2d', 'toImage']],
+    responsive: true,
+  };
+
+  Plotly.newPlot(map, [mapData, scatterData], layout, config);
+}
+
+function showLoading() {
+  console.log('Showing loading spinner...');
+  map.style.display = 'none';
+  loadingIndicator.style.display = 'block';
+}
+
+function hideLoading() {
+  console.log('Hiding loading spinner...');
+  map.style.display = 'block';
+  loadingIndicator.style.display = 'none';
 }
---
+++ webworker.js
@@ -0,0 +1,24 @@
+self.onmessage = function (e) {
+  const { xGrid, yGrid, algorithm, sigma2, alpha, x, y, z } = e.data;
+
+  importScripts('https://unpkg.com/@sakitam-gis/kriging/dist/kriging.js');
+
+  function kriging_method(model, xValue, yValue) {
+    let variogram = kriging.train(z, x, y, model, sigma2, alpha);
+    return kriging.predict(xValue, yValue, variogram);
+  }
+
+  let zGrid = yGrid.map((yVal) =>
+    xGrid.map((xVal) => {
+      if (algorithm === 'kriging (exponential)') {
+        return kriging_method('exponential', xVal, yVal);
+      } else if (algorithm === 'kriging (spherical)') {
+        return kriging_method('spherical', xVal, yVal);
+      } else if (algorithm === 'kriging (gaussian)') {
+        return kriging_method('gaussian', xVal, yVal);
+      }
+    })
+  );
+
+  self.postMessage({ zGrid });
+};
